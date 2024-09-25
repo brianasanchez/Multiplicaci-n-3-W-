0cfd4ae157d37d28ae6a910530b6eb3f327e1e3c
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-24 20:04:17 -0600

    Add files via upload

---
+++ multiplicacion.py
@@ -0,0 +1,16 @@
+print(" ") # print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-w") 
+print(" ") # print imprime un espacio
+
+def mul(a, b, c): # se da el nombre de la funcion y los argumentos
+    return (a * b * c) # indica el final de la funcion
+
+a = int(input('Ingresa el primer digito: ')) # se solicita el primer digito
+b = int(input('Ingresa el segundo digito: ')) # se solicita el segundo digito
+c = int(input('Ingresa el tercer digito: ')) # se solicita el tercer digito
+
+print(f'Multiplicacion de {a} de {b} y de {c} es {mul(a, b, c)}') # se hace la peracion de la funcion y se imprime
+
+print(" ") # print imprime un espacio
+
